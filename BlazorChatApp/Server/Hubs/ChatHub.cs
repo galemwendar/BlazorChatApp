0a3dd3597dc78f30411099aaa6417f044262a853
Андрей Петров <galemwendar@gmail.com>
2022-06-07 15:14:29 +0300

    with DateTime

--- BlazorChatApp/Server/Hubs/ChatHub.cs
+++ BlazorChatApp/Server/Hubs/ChatHub.cs
@@ -9,10 +9,10 @@ namespace BlazorChatApp.Server.Hubs
 {
 	public class ChatHub:Hub
 	{
-		public async Task SendMessage(string user, string message)
+		public async Task SendMessage(DateTime dateTime, string user, string message)
 		{
-			await Clients.All.SendAsync("ReceiveMessage", user, message);
-			Console.WriteLine($"[{user}] : {message}");
+			await Clients.All.SendAsync("ReceiveMessage", dateTime, user, message);
+			Console.WriteLine($"[{dateTime}: {user}] : {message}");
 		}
 
 	}
